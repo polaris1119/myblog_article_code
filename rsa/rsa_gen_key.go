37b191d12c59fca6aaff24bd8c39271b3186fe0f
polaris1119 <xuxinhua1984@gmail.com>
2013-01-18 18:54:10 +0800

    Go生成rsa密钥并编码写入文件 http://blog.studygolang.com/161.html

---
+++ rsa/rsa_gen_key.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"flag"
+	"log"
+	"os"
+)
+
+func main() {
+	var bits int
+	flag.IntVar(&bits, "b", 1024, "密钥长度，默认为1024位")
+	if err := GenRsaKey(bits); err != nil {
+		log.Fatal("密钥文件生成失败！")
+	}
+	log.Println("密钥文件生成成功！")
+}
+
+func GenRsaKey(bits int) error {
+	// 生成私钥文件
+	privateKey, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		return err
+	}
+	derStream := x509.MarshalPKCS1PrivateKey(privateKey)
+	block := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: derStream,
+	}
+	file, err := os.Create("private.pem")
+	if err != nil {
+		return err
+	}
+	err = pem.Encode(file, block)
+	if err != nil {
+		return err
+	}
+	// 生成公钥文件
+	publicKey := &privateKey.PublicKey
+	derPkix, err := x509.MarshalPKIXPublicKey(publicKey)
+	if err != nil {
+		return err
+	}
+	block = &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: derPkix,
+	}
+	file, err = os.Create("public.pem")
+	if err != nil {
+		return err
+	}
+	err = pem.Encode(file, block)
+	if err != nil {
+		return err
+	}
+	return nil
+}
